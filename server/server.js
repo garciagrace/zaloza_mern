02805f9a3f22c063e3c3fb4f29a2af230c508919
Grace Garcia <gracegarcia0803@gmail.com>
2021-08-25 15:42:53 +0800

    Added custom error handler middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
@@ -16,6 +17,9 @@ app.use('/api/products', productRoutes);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
