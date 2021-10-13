c22844216f74e21203af0f128ebd52f2e515c409
Grace Garcia <gracegarcia0803@gmail.com>
2021-10-13 16:46:50 +0800

    Added get user profile controller and route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -66,4 +66,26 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      contactNum: user.contactNum,
+      email: user.email,
+      address: user.address,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, registerUser, getUserProfile };
