ba00f96eafaef3cd7dbc8e9b6bf4d112d45c1d6b
Grace Garcia <gracegarcia0803@gmail.com>
2021-10-17 20:35:35 +0800

    Add user register acttion and reducer

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -39,3 +39,45 @@ export const logout = () => (dispatch) => {
   dispatch({ type: 'USER_LOGOUT' });
   document.location.href = '/';
 };
+
+// Register user
+export const register =
+  (firstName, lastName, contactNum, email, password) => async (dispatch) => {
+    try {
+      dispatch({
+        type: 'USER_REGISTER_REQUEST',
+      });
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const { data } = await axios.post(
+        '/api/users',
+        { firstName, lastName, contactNum, email, password },
+        config
+      );
+
+      dispatch({
+        type: 'USER_REGISTER_SUCCESS',
+        payload: data,
+      });
+
+      dispatch({
+        type: 'USER_LOGIN_SUCCESS',
+        payload: data,
+      });
+
+      localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (error) {
+      dispatch({
+        type: 'USER_REGISTER_FAIL',
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -7,6 +7,24 @@ export const userLoginReducer = (state = {}, action) => {
       return { loading: false, userInfo: action.payload };
     case 'USER_LOGIN_FAIL':
       return { loading: false, error: action.payload };
+    case 'USER_LOGOUT':
+      return {};
+    default:
+      return state;
+  }
+};
+
+// User register
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'USER_REGISTER_REQUEST':
+      return { loading: true };
+    case 'USER_REGISTER_SUCCESS':
+      return { loading: false, userInfo: action.payload };
+    case 'USER_REGISTER_FAIL':
+      return { loading: false, error: action.payload };
+    case 'USER_LOGOUT':
+      return {};
     default:
       return state;
   }
