65f633211a4e3e45edda3ca3ef60d43f8ee332a4
Grace Garcia <gracegarcia0803@gmail.com>
2022-01-07 10:04:17 +0800

    Added create order controller and route

---
+++ server/controllers/orderController.js
@@ -0,0 +1,34 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    shippingPrice,
+    totalPrice,
+    paymentMethod,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+    return;
+  } else {
+    const order = new Order({
+      user: req.user._id,
+      orderItems,
+      shippingAddress,
+      shippingPrice,
+      totalPrice,
+      paymentMethod,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
