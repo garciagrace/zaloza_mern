d90915b7927fb08a5d797261ab1e2eb06794459d
Grace Garcia <gracegarcia0803@gmail.com>
2022-01-21 10:38:19 +0800

    Added paypal config on server

--- server/server.js
+++ server/server.js
@@ -20,6 +20,10 @@ app.use('/api/users', userRoutes);
 app.use('/api/carts', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
