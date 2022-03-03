226699362337a33a3de972a7624d4ce080ff19ba
Grace Garcia <gracegarcia0803@gmail.com>
2022-03-03 11:41:43 +0800

    Added admin -  delete product

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -6,12 +6,16 @@ import {
   getAllProducts,
   createProduct,
   updateProduct,
+  deleteProduct,
 } from '../controllers/productController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 
 router.route('/:category/').get(getProductsByCategory);
 router.route('/:category/:id').get(getProductById);
 router.route('/').get(getAllProducts).post(protect, admin, createProduct);
-router.route('/:id').put(protect, admin, updateProduct);
+router
+  .route('/:id')
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
