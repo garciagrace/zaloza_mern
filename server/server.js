26dcbedb3d88eabd2254ff2be8c2530167ec5531
Grace Garcia <gracegarcia0803@gmail.com>
2022-05-22 19:46:58 +0800

    Prepare for deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import path from 'path';
+import path, { dirname } from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 
@@ -27,12 +27,20 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
